aeeab45edce86599c3e1c9fdae761169c825874d
aziz-kacho <76108391+aziz-kacho@users.noreply.github.com>
2024-05-28 21:23:21 +0500

    add to gitignore

--- src/main/kotlin/it/skills/itskills/constants.kt
+++
@@ -1,8 +0,0 @@
-package it.skills.itskills
-
-const val CHAT_GPT_API = "https://api.openai.com/"
-const val CHAT_GPT_API_TOKEN =
-const val CHAT_GPT_MODEL =
-
-const val TELEGRAM_BOT_USERNAME =
-const val TELEGRAM_BOT_TOKEN =
