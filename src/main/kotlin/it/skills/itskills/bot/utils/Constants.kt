a0825e7390d833883864207b0bf237293206e6c9
aziz-kacho <76108391+aziz-kacho@users.noreply.github.com>
2024-05-28 21:24:35 +0500

    rm history in Constants

--- src/main/kotlin/it/skills/itskills/bot/utils/Constants.kt
+++ src/main/kotlin/it/skills/itskills/bot/utils/Constants.kt
@@ -1,16 +1,6 @@
 package it.skills.itskills.bot.utils
 
 object Constants {
-    //    const val TELEGRAM_BOT_USERNAME = "ItSkillsSchoolBot"
-//    const val TELEGRAM_BOT_TOKEN = "5440267997:AAFCleAW15CtxDMpLNgJwGc1mMnyv7Ebljc"
-
-
-
-
-
-
     const val HttpExceptionError = "Ой что-то пошло не так"
     const val  IOExceptionError = "Ошибка сервера, проверьте подключение к интернету"
-
-
 }
