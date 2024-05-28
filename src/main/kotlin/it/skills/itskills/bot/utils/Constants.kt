22ec08d59da7c7e7b47eb0fbea88991dea426bdb
aziz-kacho <76108391+aziz-kacho@users.noreply.github.com>
2024-05-28 21:09:33 +0500

    create file constants.kt

--- src/main/kotlin/it/skills/itskills/bot/utils/Constants.kt
+++ src/main/kotlin/it/skills/itskills/bot/utils/Constants.kt
@@ -4,14 +4,13 @@ object Constants {
     //    const val TELEGRAM_BOT_USERNAME = "ItSkillsSchoolBot"
 //    const val TELEGRAM_BOT_TOKEN = "5440267997:AAFCleAW15CtxDMpLNgJwGc1mMnyv7Ebljc"
 
-    const val TELEGRAM_BOT_USERNAME = "ChatGptCodeHubBot"
-    const val TELEGRAM_BOT_TOKEN = "6034790014:AAE6HvGlejlMH4dl67BeW9imaR62dJuUkRg"
 
-    const val CHAT_GPT_API = "https://api.openai.com/"
-    const val CHAT_GPT_API_TOKEN = "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"
+
+
+
 
     const val HttpExceptionError = "Ой что-то пошло не так"
     const val  IOExceptionError = "Ошибка сервера, проверьте подключение к интернету"
 
-    const val CHAT_GPT_MODEL = "gpt-3.5-turbo"
+
 }
---
+++ src/main/kotlin/it/skills/itskills/constants.kt
@@ -0,0 +1,8 @@
+package it.skills.itskills
+
+const val CHAT_GPT_API = "https://api.openai.com/"
+const val CHAT_GPT_API_TOKEN = Your open-ai token
+const val CHAT_GPT_MODEL = chat gpt model
+
+const val TELEGRAM_BOT_USERNAME = telegram bot name
+const val TELEGRAM_BOT_TOKEN = telegram bot token
